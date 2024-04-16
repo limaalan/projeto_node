429e96ee697988b2e081631df10e58182c1e6e60
Alan Lima <alanlimamarques6@gmail.com>
2024-04-16 11:32:09 -0300

    feat: add elsint config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,6 +2,10 @@ import express from 'express' ;
 
 const server = express();
 
+interface Teste {
+    
+}
+
 //Configurações do servidor vão aqui
 server.get('/', (req,res)=>{
     return res.send ("Hello world!");
