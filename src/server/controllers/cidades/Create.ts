315b46467462c85726fb0ab20a60a98e50e87100
Alan Lima <alanlimamarques6@gmail.com>
2024-04-18 11:34:04 -0300

    Feat : Validação cidade

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 
 interface ICidade {
     nome : string ;
+    estado : string ;
 }
 
-export const create = (req: Request<{},{},ICidade > , res: Response) => {
+const bodyValidation:yup.ObjectSchema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3) ,
+    estado : yup.string().required().min(3),
+});
 
-    const data:ICidade = req.body;
-    console.log(data.nome);
+
+export const create = async (req: Request<{},{},ICidade > , res: Response) => {
+    let validatedData: ICidade | undefined = undefined ;
+   try {
+        validatedData = await bodyValidation.validate(req.body, {abortEarly:false});
+   } catch (error) {
+        const yupError = error as yup.ValidationError;
+        const ValidationError : Record<string, string> = {}
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors:{
+                default : yupError.message,
+            }
+        });
+   }
+   
+    console.log(validatedData);
     
     return res.send("Create!");
 }
