de79d8f96fb124e5ef01e6f9b40e33a39440842f
Alan Lima <alanlimamarques6@gmail.com>
2024-05-14 11:31:19 -0300

    feat:criar migrations de cidade, configurar scritps do knex

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+
+export async function up(knex: Knex) {
+    return knex
+    .schema
+    .createTable(ETableNames.cidade,table =>{
+        table.bigIncrements('id').primary().index();
+        table.string('nome',150).index().notNullable();
+        table.comment('Tabela de Cidades do sistema') // Comentário 
+    }).then(()=>{
+        console.log(`#Create table ${ETableNames.cidade}`); // Log no terminal para confirmar que deu certo a migrate
+    })
+}
+
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable(ETableNames.cidade);
+}
+
