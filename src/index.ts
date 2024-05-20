146c351fd8dca2b4ef20406b39136cc7677d66f7
Alan Lima <alanlimamarques6@gmail.com>
2024-05-20 09:53:26 -0300

    fix: executar migrations ao inicar o servidor em produção, rearranjar dependências

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
-server.listen(process.env.PORT|| 3333, ()=> {    
+const startServer = ()=>{
+    server.listen(process.env.PORT|| 3333, ()=> {    
     console.log(`App rodando na porta ${process.env.PORT || 3333 }`)
-});
+})};
+
+if(process.env.IS_LOCAL_HOST !== 'true'){
+    Knex.migrate.latest()
+    .then(()=>{
+        startServer();
+    })
+    .catch(console.log);
+} else {
+    startServer();
+}
